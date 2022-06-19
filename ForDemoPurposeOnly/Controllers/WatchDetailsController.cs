89772f8ab0f8774848aa0c4d0c7c5f3e076970a6
romar.gamboa <romargamboa28@gmail.com>
2022-06-20 04:46:36 +0800

    Commit and push Watch Catalog Website

---
+++ ForDemoPurposeOnly/Controllers/WatchDetailsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ForDemoPurposeOnly.Models;
+
+namespace ForDemoPurposeOnly.Controllers
+{
+    public class WatchDetailsController : Controller
+    {
+        private readonly WatchDetailContext _context;
+
+        public WatchDetailsController(WatchDetailContext context)
+        {
+            _context = context;
+        }
+
+        // GET: WatchDetails
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.WatchDetails.ToListAsync());
+        }
+
+        // GET: WatchDetails/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var watchDetails = await _context.WatchDetails
+                .FirstOrDefaultAsync(m => m.WatchId == id);
+            if (watchDetails == null)
+            {
+                return NotFound();
+            }
+
+            return View(watchDetails);
+        }
+
+        // GET: WatchDetails/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: WatchDetails/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("WatchId,WatchName,SerialNo,Price,WatchWeight,WatchHeight,WatchDiameter,WatchThickness,ShortDesc,Caliber,Movement,Chronograph,Jewel,CaseMaterial,StrapMaterial,FullDesc")] WatchDetails watchDetails)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(watchDetails);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(watchDetails);
+        }
+
+        // GET: WatchDetails/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var watchDetails = await _context.WatchDetails.FindAsync(id);
+            if (watchDetails == null)
+            {
+                return NotFound();
+            }
+            return View(watchDetails);
+        }
+
+        // POST: WatchDetails/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("WatchId,WatchName,SerialNo,Price,WatchWeight,WatchHeight,WatchDiameter,WatchThickness,ShortDesc,Caliber,Movement,Chronograph,Jewel,CaseMaterial,StrapMaterial,FullDesc")] WatchDetails watchDetails)
+        {
+            if (id != watchDetails.WatchId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(watchDetails);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WatchDetailsExists(watchDetails.WatchId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(watchDetails);
+        }
+
+        // GET: WatchDetails/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var watchDetails = await _context.WatchDetails
+                .FirstOrDefaultAsync(m => m.WatchId == id);
+            if (watchDetails == null)
+            {
+                return NotFound();
+            }
+
+            return View(watchDetails);
+        }
+
+        // POST: WatchDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var watchDetails = await _context.WatchDetails.FindAsync(id);
+            _context.WatchDetails.Remove(watchDetails);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WatchDetailsExists(int id)
+        {
+            return _context.WatchDetails.Any(e => e.WatchId == id);
+        }
+    }
+}
---
+++ ForDemoPurposeOnly/Data/WatchDetailContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace ForDemoPurposeOnly.Models
+{
+    public partial class WatchDetailContext : DbContext
+    {
+        public WatchDetailContext()
+        {
+        }
+
+        public WatchDetailContext(DbContextOptions<WatchDetailContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<WatchDetails> WatchDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:demoproj062122.database.windows.net,1433;Initial Catalog=DemoProject062122;Persist Security Info=False;User ID=romargamboa;Password=@ccenture1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WatchDetails>(entity =>
+            {
+                entity.HasKey(e => e.WatchId)
+                    .HasName("PK__WatchDet__3BA3DA83346F4FCE");
+
+                entity.Property(e => e.WatchId).HasColumnName("WatchID");
+
+                entity.Property(e => e.Caliber).HasMaxLength(50);
+
+                entity.Property(e => e.CaseMaterial).HasMaxLength(50);
+
+                entity.Property(e => e.Chronograph).HasMaxLength(50);
+
+                entity.Property(e => e.FullDesc).HasMaxLength(150);
+
+                entity.Property(e => e.Jewel).HasMaxLength(50);
+
+                entity.Property(e => e.Movement).HasMaxLength(50);
+
+                entity.Property(e => e.ShortDesc).HasMaxLength(50);
+
+                entity.Property(e => e.StrapMaterial).HasMaxLength(50);
+
+                entity.Property(e => e.WatchName).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
