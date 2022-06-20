52e0584e7d35dd46bc68879b6a57f96a405b36bb
romar.gamboa <romargamboa28@gmail.com>
2022-06-20 12:19:10 +0800

    Create a search service, carousel and CRUD
    Related Work Items: #59, #60, #63

---
+++ ForDemoPurposeOnly/Controllers/PagesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ForDemoPurposeOnly.Views.Pages
+{
+    public class PagesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Product()
+        {
+            return View();
+        }
+    }
+}
